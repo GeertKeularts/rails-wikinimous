714dc1589b2506e2be767106ffecfe2cea35148e
Geert Keularts <geertkeularts@gmail.com>
2016-06-01 11:09:37 +0200

    added redcarpet

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
+require 'redcarpet'
+
 class ArticlesController < ApplicationController
+
+
   def index
     @articles = Article.all
   end
@@ -12,14 +16,30 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save!
+    redirect_to articles_path
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
